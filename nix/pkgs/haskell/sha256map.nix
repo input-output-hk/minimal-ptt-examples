18fbaad491b71685e0e1b460ed34ea78410d5cb6
Alasdair Hill <alasdair.hill@iohk.io>
2023-04-25 13:21:37 +0100

    just cabal without nix

--- nix/pkgs/haskell/sha256map.nix
+++
@@ -1,9 +0,0 @@
-{
-  "https://github.com/input-output-hk/cardano-addresses"."b7273a5d3c21f1a003595ebf1e1f79c28cd72513" = "129r5kyiw10n2021bkdvnr270aiiwyq58h472d151ph0r7wpslgp";
-  "https://github.com/input-output-hk/cardano-config"."1646e9167fab36c0bff82317743b96efa2d3adaa" = "sha256-TNbpnR7llUgBN2WY7CryMxNVupBIUH01h1hRNHoxboY=";
-  "https://github.com/input-output-hk/cardano-ledger"."da3e9ae10cf9ef0b805a046c84745f06643583c2" = "sha256-3VUZKkLu1R43GUk9IwgsGQ55O0rnu8NrCkFX9gqA4ck=";
-  "https://github.com/input-output-hk/cardano-wallet"."18a931648550246695c790578d4a55ee2f10463e" = "0i40hp1mdbljjcj4pn3n6zahblkb2jmpm8l4wnb36bya1pzf66fx";
-  "https://github.com/sevanspowell/hw-aeson"."b5ef03a7d7443fcd6217ed88c335f0c411a05408" = "1dwx90wqavdl4d0npbzbxyh2pzi9zs1qz7nvsrb3n1cm2xbv4i5z";
-  "https://github.com/input-output-hk/quickcheck-contractmodel"."a8a952503d3d6d9bbb9dd5f26b8a4b6769a175f1" = "sha256-3vwkS7bJRcBpqUOOsYrgqMYnOjHpPlJUeMK9+gX9nCo=";
-  "https://github.com/input-output-hk/plutus-apps.git"."5c54f209322f62b73d88150ecb5d781d515de746" = "sha256-krHvT653ngPWRaB0XVfupCkLUnohhmymjG6Vm9bawv0=";
-}
