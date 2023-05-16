ff19aabe62ba6d43a6a91e45dce567431ecd7471
Alasdair Hill <alihill77@gmail.com>
2023-05-16 16:12:12 +0100

    fix hash

--- sha256map.nix
+++ sha256map.nix
@@ -1,3 +1,3 @@
 {
-  "https://github.com/input-output-hk/plutus-apps.git"."5c54f209322f62b73d88150ecb5d781d515de746" = "krHvT653ngPWRaB0XVfupCkLUnohhmymjG6Vm9bawv0";
+  "https://github.com/input-output-hk/plutus-apps.git"."5c54f209322f62b73d88150ecb5d781d515de746" = "krHvT653ngPWRaB0XVfupCkLUnohhmymjG6Vm9bawv0=";
 }
